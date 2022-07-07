2a19cf4d359d2bf26d20bba0e599361418e94a0d
a5748783 <a5748783@gmail.com>
2022-07-07 16:49:31 +0800

    feat: add mysql

--- app.js
+++ app.js
@@ -10,9 +10,37 @@ app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+// 路由設定
 app.get('/', (req, res) => {
   res.send('hello world')
 })
+
+// 登入頁面
+app.get('/users/login', (req, res) =>{
+  res.render('login')
+})
+
+// 登入檢查
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+// 註冊頁面
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+// 註冊檢查
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+
+// 登出
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
+})
+
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
